3820b06172f16e64b7ef9f2a6599cfa102a75c14
mjarvihaavisto <60304828+maksutheam@users.noreply.github.com>
2022-01-23 18:25:12 +0200

    stuff

--- Ocean/LoseScreen.gd
+++ Ocean/LoseScreen.gd
@@ -5,6 +5,21 @@ onready var reason = $CanvasLayer/Control/reason
 
 func _ready():
 	reason.text = global.lose_reason
+	global.money = 5 # players money
+	global.danger = 0 # players notoriety
+	global.max_danger = 1
+	global.min_danger = 0
+	global.day = 0
+	global.allies = 0
+	global.food = 3
+	global.one_food_left = false
+	global.investors_share = 0
+	global.current_target = TargetShip.new()
+	#weird hack but whatever lmao
+	global.target_selected = false
+	global.loot_gained = 0
+	global.show_tutorial = true
+
 
 
 func _on_Button_pressed():
---
+++ Ocean/SeaBG.gd
@@ -0,0 +1,5 @@
+extends ParallaxBackground
+
+
+func _process(delta):
+	scroll_offset += Vector2.LEFT
